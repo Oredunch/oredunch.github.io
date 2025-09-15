f3c5f137d6db904757570e176fef8f097961d695
Oredunch <hyllypokemon@icloud.com>
2025-09-15 10:49:42 +0300

    Updated index.html, added javascript, updated mobile animations in styles.css

---
+++ scripts.js
@@ -0,0 +1,24 @@
+const logo=document.querySelector('.logo');
+let isSpinning=false;
+
+if(window.matchMedia('(hover: none)').matches){
+  logo.addEventListener('touchstart',e=>{
+    e.preventDefault();//prevent scroll
+    if(isSpinning)return;
+    isSpinning=true;
+    logo.style.filter='none';
+    logo.style.transform='none';
+    void logo.offsetWidth;//reflow
+    logo.classList.add('spin');
+  },{passive:false});
+
+  logo.addEventListener('animationend',()=>{
+    isSpinning=false;
+    logo.classList.remove('spin');
+    logo.style.filter='none';
+    logo.style.transform='none';
+  });
+}
+
+
+// this was so hard and frustrating im never doing this again
