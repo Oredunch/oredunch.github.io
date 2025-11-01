6eca93593b66faddcbba95ff84660887990c233b
SamOredunchinch <137100806+Oredunch@users.noreply.github.com>
2025-11-01 23:16:37 +0200

    Updated layout

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const logo = document.querySelector('.logo');
+const buttonTapHover = document.getElementById('button');
 let isSpinning = false;
 
 function spinLogo(e) {
@@ -15,12 +16,20 @@ function spinLogo(e) {
 function endSpin() {
 	isSpinning = false;
 	logo.classList.remove('spin');
-	logo.style.filter = 'drop-shadow(5px 15px 5px #000A) blur(0)';
+	logo.style.filter = 'drop-shadow(0px 0px 10px #07556955) blur(0px)';
 	logo.style.cursor = 'pointer';
 	logo.style.transform = 'none';
 	logo.offsetHeight;
 	logo.style.transform = 'translateZ(0)'; //nudge gpu
 }
 
+function buttonEffect() {
+	buttonTapHover.classList.add('activated');
+	setTimeout(() => {
+		buttonTapHover.classList.remove('activated');
+	}, 200);
+}
+
 logo.addEventListener('click', spinLogo);
 logo.addEventListener('animationend', endSpin);
+buttonTapHover.addEventListener('touchstart', buttonEffect);
