410af73eb68f3b81a05ea459a34df8fc354a6edb
SamOredunchinch <137100806+Oredunch@users.noreply.github.com>
2025-11-01 23:56:53 +0200

    Changed animation, styles.css, scripts.js

--- scripts.js
+++ scripts.js
@@ -1,35 +1,25 @@
 const logo = document.querySelector('.logo');
-const buttonTapHover = document.getElementById('button');
-let isSpinning = false;
+const button = document.getElementById('button');
+var isSpinning = false;
 
-function spinLogo(e) {
-	e.preventDefault();
+function spinLogo() {
 	if (isSpinning) return;
 	isSpinning = true;
-	logo.style.filter = 'none';
-	logo.style.transform = 'none';
-	logo.style.cursor = 'default';
-	void logo.offsetWidth;
 	logo.classList.add('spin');
 }
 
 function endSpin() {
 	isSpinning = false;
 	logo.classList.remove('spin');
-	logo.style.filter = 'drop-shadow(0px 0px 10px #07556955) blur(0px)';
-	logo.style.cursor = 'pointer';
-	logo.style.transform = 'none';
-	logo.offsetHeight;
-	logo.style.transform = 'translateZ(0)'; //nudge gpu
 }
 
 function buttonEffect() {
-	buttonTapHover.classList.add('activated');
+	button.classList.add('activated');
 	setTimeout(() => {
-		buttonTapHover.classList.remove('activated');
+		button.classList.remove('activated');
 	}, 200);
 }
 
 logo.addEventListener('click', spinLogo);
 logo.addEventListener('animationend', endSpin);
-buttonTapHover.addEventListener('touchstart', buttonEffect);
+button.addEventListener('touchstart', buttonEffect);
