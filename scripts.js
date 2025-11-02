ad1d3194c1d409fa076dca887f39e8a706980361
SamOredunchinch <137100806+Oredunch@users.noreply.github.com>
2025-11-02 17:03:22 +0200

    Added interactive color change to name, modified interaction animation of logo to use opacity instead of filter: opacity

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
 const logo = document.querySelector('.logo');
 const button = document.getElementById('button');
-var isSpinning = false;
+const samName = document.getElementById('name');
 
-function spinLogo() {
+var isSpinning = false;
+var imaginary = 0;
+var samsName = 'Sammy Oredunchinch';
+var speed = 50;
+
+function spinLogo() { // Its not a spin animation anymore tho but whatever
 	if (isSpinning) return;
 	isSpinning = true;
 	logo.classList.add('spin');
@@ -20,6 +25,35 @@ function buttonEffect() {
 	}, 200);
 }
 
+function nameTypeWrite() {
+	if (imaginary < samsName.length) {
+		let textArray = samName.textContent.split('');
+		textArray[imaginary] = samsName.charAt(imaginary);
+		samName.textContent = textArray.join('');
+		imaginary++;
+		setTimeout(nameTypeWrite, speed);
+	}
+}
+
+function randomizeColor() {
+	const textColor = [
+		'#c3c382',
+		'#FF00FF',
+		'#4ae39eff',
+		'#ff0000ff',
+		'#c0830aff',
+	];
+	document.documentElement.style.setProperty("--text-color", textColor[Math.floor(Math.random() * textColor.length)]);
+	setTimeout(() => {
+		document.documentElement.style.setProperty("--text-color", "#00FFFF");
+	}, 1000);
+}
+
 logo.addEventListener('click', spinLogo);
 logo.addEventListener('animationend', endSpin);
 button.addEventListener('touchstart', buttonEffect);
+samName.addEventListener('click', randomizeColor);
+
+document.addEventListener("DOMContentLoaded", () => {
+	nameTypeWrite();
+});
