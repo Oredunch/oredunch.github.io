ef35f936490c0800ac7845549925406c2cb03700
SamOredunchinch <137100806+Oredunch@users.noreply.github.com>
2025-11-02 20:38:58 +0200

    I don't need to tell myself what i did

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 const logo = document.querySelector('.logo');
 const button = document.getElementById('button');
 const samName = document.getElementById('name');
+const videoBackground = document.getElementById('back-anim');
+const videoPrefsCheck = document.getElementById('video-check');
+const indicatorBlob = document.getElementById('video-check-indicator');
+const tellText = document.getElementById('telltext');
 
 var isSpinning = false;
 var imaginary = 0;
@@ -35,6 +39,14 @@ function nameTypeWrite() {
 	}
 }
 
+function updateVideoStuffs() {
+	videoBackground.classList.toggle('no-opacity', !videoPrefsCheck.checked);
+	setTimeout(() => {
+		videoBackground.classList.toggle('b-a-hidden', !videoPrefsCheck.checked);
+	}, 500);
+	indicatorBlob.classList.toggle('check-hidden', !videoPrefsCheck.checked);
+}
+
 function randomizeColor() {
 	const textColor = [
 		'#c3c382',
@@ -43,9 +55,9 @@ function randomizeColor() {
 		'#ff0000ff',
 		'#c0830aff',
 	];
-	document.documentElement.style.setProperty("--text-color", textColor[Math.floor(Math.random() * textColor.length)]);
+	document.documentElement.style.setProperty('--text-color', textColor[Math.floor(Math.random() * textColor.length)]);
 	setTimeout(() => {
-		document.documentElement.style.setProperty("--text-color", "#00FFFF");
+		document.documentElement.style.setProperty('--text-color', '#00FFFF');
 	}, 1000);
 }
 
@@ -53,7 +65,11 @@ logo.addEventListener('click', spinLogo);
 logo.addEventListener('animationend', endSpin);
 button.addEventListener('touchstart', buttonEffect);
 samName.addEventListener('click', randomizeColor);
+videoPrefsCheck.addEventListener('change', updateVideoStuffs);
 
-document.addEventListener("DOMContentLoaded", () => {
-	nameTypeWrite();
+document.addEventListener('DOMContentLoaded', () => {
+	videoPrefsCheck.checked = true;
+	videoBackground.classList.remove('b-a-hidden');
+	indicatorBlob.classList.remove('check-hidden');
+	setTimeout(() => { nameTypeWrite() }, 200);
 });
