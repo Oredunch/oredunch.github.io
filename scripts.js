c30810c9c09454c645d194eb97d536da34ecadfd
SamOredunchinch <137100806+Oredunch@users.noreply.github.com>
2025-11-03 16:10:06 +0200

    Small tweaks

--- scripts.js
+++ scripts.js
@@ -10,6 +10,8 @@ const infoToggle = document.getElementById("info-toggle");
 const popLabel = document.getElementById("pop-label");
 const centerPrimary = document.getElementById("center-1");
 const clickSpace = document.getElementById("clickspace");
+const htmlEl = document.documentElement;
+const bodyEl = document.body;
 
 const line1 = document.getElementById("labelline");
 const line2 = document.getElementById("labelline2");
@@ -20,6 +22,15 @@ var isSpinning = false;
 var imaginary = 0;
 var samsName = 'Sammy Oredunchinch';
 var speed = 50;
+
+const eventTw = document.createEvent("Event");
+
+eventTw.initEvent("aftertypewrite", true, true);
+
+function showPage() {
+	htmlEl.classList.add('opaq');
+	bodyEl.classList.add('opaq');
+}
 
 function spinLogo() { // Its not a spin animation anymore tho but whatever
 	if (isSpinning) return;
@@ -48,6 +59,14 @@ function nameTypeWrite() {
 		setTimeout(nameTypeWrite, speed);
 	}
 }
+
+function flashUnderLine() {
+	samName.classList.add('underline');
+	setTimeout(() => {
+		samName.classList.remove('underline');
+	}, 500);
+}
+
 
 function popOut() {
 	infoTab.classList.toggle('poppedout', !infoToggle.checked);
@@ -84,9 +103,9 @@ function randomizeColor() {
 		'#ff0000ff',
 		'#c0830aff',
 	];
-	document.documentElement.style.setProperty('--text-color', textColor[Math.floor(Math.random() * textColor.length)]);
+	document.documentElement.style.setProperty('--sam-color', textColor[Math.floor(Math.random() * textColor.length)]);
 	setTimeout(() => {
-		document.documentElement.style.setProperty('--text-color', '#00FFFF');
+		document.documentElement.style.setProperty('--sam-color', '#00FFFF');
 	}, 1000);
 }
 
@@ -94,17 +113,21 @@ logo.addEventListener('click', spinLogo);
 logo.addEventListener('animationend', endSpin);
 button.addEventListener('touchstart', buttonEffect);
 samName.addEventListener('click', randomizeColor);
+samName.addEventListener('aftertypewrite', flashUnderLine);
 videoPrefsCheck.addEventListener('change', updateVideoStuffs);
 infoToggle.addEventListener('change', popOut);
 clickSpace.addEventListener('touchstart', clickToReturn);
 
 document.addEventListener('DOMContentLoaded', () => {
-	clickSpace.classList.add('disabled');
-	infoToggle.checked = true;
-	videoPrefsCheck.checked = true;
-	videoBackground.classList.remove('b-a-hidden');
-	indicatorBlob.classList.remove('check-hidden');
-	setTimeout(() => { nameTypeWrite() }, 200);
+	showPage();
+	setTimeout(() => {
+		infoToggle.checked = true;
+		videoPrefsCheck.checked = true;
+		clickSpace.classList.add('disabled');
+		videoBackground.classList.remove('b-a-hidden');
+		indicatorBlob.classList.remove('check-hidden');
+		setTimeout(() => { nameTypeWrite(); }, 200);
+	}, 250);
 });
 
 fetch('/latest-video.json')
@@ -113,9 +136,9 @@ fetch('/latest-video.json')
 		const url = `https://www.youtube.com/watch?v=${data.videoId}`;
 		videoInfo.innerHTML = `<a href="${url}" target="_blank">${data.title}</a>`;
 		if (data.title == "") {
-			videoInfo.innerHTML = "Error Loading";
-			videoInfo.classList.add('error-format');
-			
+			videoInfo.innerHTML = `<p class="error-format" title="Sometimes this shows if the API is not responding or the GitHub Workflow has an error." >&#10007; Error loading video.</p>`;
+			videoInfo.classList.remove('ax');
+
 		}
 	})
 	.catch(err => {
